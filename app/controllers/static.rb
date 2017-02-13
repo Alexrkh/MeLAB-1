1007f7064201fdf08fb2cedff1ffb4640b40ffbd
Bong <mjbond_bong2@yahoo.com>
2017-02-13 11:16:20 +0800

    add static

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,41 @@
 get '/' do
+  @error = params[:x]
   erb :"static/index"
 end
+
+get '/signup' do
+	erb :"static/signup"
+end
+
+post '/signup' do
+	if params[:password] == params[password_comfirmation]
+	user = User.new(:name=>params[:username],:email=>params[:email],:password=>params[:password],)
+		if user.save
+		redirect 'sessions/login'
+    	else
+    	redirect '/? x= signup fail'
+		end
+	redirect '/? x= signup fail'
+end
+
+
+post '/login' do
+	user = User.find_by(:username=>params[:username])
+	if user && user.authenticate(params[:password])
+		session[:user_id] = user.id
+	redirect 'static/question'
+    else
+    redirect '/? x= signup fail'
+	end
+end
+
+get '/sessions' do
+	 erb 'static/question'
+end
+
+get '/sessions/logout' do
+	session.clear
+	redirect '/'
+
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
+  include BCrypt
+  attr_reader :name
   has_many :answers
   has_many :questions
   has_secure_password
 
-def self.password()
-	@password = params[:password]
 
-	user = User.new
-
-	#take the password from the form and save here.
 end
