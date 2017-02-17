1cb6c169d38a756d991730e1ca43e2d69c8d0aab
Bong <mjbond_bong2@yahoo.com>
2017-02-17 14:57:35 +0800

    done ajak

--- public/js/application.js
+++ public/js/application.js
@@ -10,13 +10,31 @@ form.submit(function(event) {
     dataType: "JSON",
     data: form.serialize(),
     success:function(data){
+    
       // successful request; do something with the data
-      debugger;
-      $('.table').append("<tr><th scope=row>1</th><td>"+ data.created_at + "</td><td><a href=/questions/"+ data.id + ">" + data.text + "</a></td></td></tr>")<td><form action="/questions/"+ data.id +"/destroy" method="post"><button class="btn btn-lg btn-primary btn-block" type="submit">Delete</button></form></td>
+      var del_btn_path = "/questions/" + data.id + "/destroy";
+      $('.table').append("<tr><th scope=row>1</th><td>"+ data.created_at + "</td><td><a href=/questions/"+ data.id + ">" + data.text + "</a></td></td>" + '<td><form action="' + del_btn_path + '" method="post"><button class="btn btn-lg btn-primary btn-block" type="submit">Delete</button></form></td></tr>')
     },
     error:function(){
       // failed request; give feedback to user
       $('#ajax-panel').html('<p class="error"><strong>Oops!</strong> Try that again in a few moments.</p>');
+    }
+  });
+});
+
+($'.del-btn').submit(function(event) {
+  // Get the question_id
+  event.preventDefault();
+    $.ajax({
+    type: 'POST',
+    url: '/questions/' + question_id + 'destroy',
+    dataType: "JSON",
+    data: form.serialize(),
+    success:function(data){
+
+    }
+    error:function(){
+
     }
   });
 });
